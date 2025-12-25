325f944ec506b2b691ce5ecb8c9969e7eb82cb4f
praneeth1984 <praneeth1984@gmail.com>
2025-12-25 19:50:07 +0530

    fix: explicitly add cloud ingestPoint for OpenReplay

--- src/components/OpenReplayTracker.tsx
+++ src/components/OpenReplayTracker.tsx
@@ -5,6 +5,7 @@ import OpenReplay from '@openreplay/tracker/cjs';
 
 const tracker = new OpenReplay({
   projectKey: process.env.NEXT_PUBLIC_OPENREPLAY_PROJECT_KEY || "IFI2kimWvLpr7KkSq6Bm",
+  ingestPoint: "https://api.openreplay.com/ingest",
 });
 
 export default function OpenReplayTracker() {
