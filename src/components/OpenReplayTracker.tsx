e02c9093cf6b0c127616e8e8ecb9ee1eb31260e0
praneeth1984 <praneeth1984@gmail.com>
2025-12-25 19:53:59 +0530

    Fixed open replay bug

--- src/components/OpenReplayTracker.tsx
+++ src/components/OpenReplayTracker.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-import OpenReplay from '@openreplay/tracker/cjs';
+import OpenReplay from '@openreplay/tracker';
 
 const tracker = new OpenReplay({
   projectKey: process.env.NEXT_PUBLIC_OPENREPLAY_PROJECT_KEY || "IFI2kimWvLpr7KkSq6Bm",
